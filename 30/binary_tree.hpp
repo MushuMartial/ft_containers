c188079ec8690d2378278e5ca46bba335e5be334
Mushu <92597474+MushuMartial@users.noreply.github.com>
2022-07-30 13:44:56 +0200

    Add files via upload

---
+++ 30/binary_tree.hpp
@@ -0,0 +1,429 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   binary_tree.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmartial <tmartial@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/02 11:52:33 by tmartial          #+#    #+#             */
+/*   Updated: 2022/07/29 11:43:05 by tmartial         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef BINARY_TREE_HPP
+# define BINARY_TREE_HPP
+
+# include "utils.hpp"
+
+namespace ft
+{
+	template<class Key, class T>
+	struct node
+	{
+		public:
+			node*					parent;
+			node*					left;
+			node*					right;
+			ft::pair<const Key, T>*	data;
+		
+			//Constructor Default
+			node() : parent(NULL), left(NULL), right(NULL), data(NULL)
+			{
+
+			}
+
+			node(const ft::pair<const Key, T>& val) : parent(NULL), left(NULL), right(NULL)
+			{
+				std::allocator<ft::pair<const Key,T> >	alloc;
+
+				this->data = alloc.allocate(1);
+				alloc.construct(this->data, val);
+			}
+			
+			//Destructor
+			~node()
+			{
+				// std::allocator<pair<const Key,T> >	alloc;
+
+				// if (this->data)
+				// {
+				// 	alloc.destroy(this->data);
+				// 	alloc.deallocate(data, 1);
+				// }
+			}
+	};
+	
+	template < class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator<ft::pair<const Key,T> > >
+	class tree
+	{
+		public:
+			// Members Types
+			typedef	Key											key_type;
+			typedef	T											mapped_type;
+			typedef	ft::pair<const key_type, mapped_type>		value_type;
+			typedef	Compare										key_compare;
+			typedef	Alloc										allocator_type;
+			typedef	typename allocator_type::reference			reference;
+			typedef	typename allocator_type::const_reference	const_reference;
+			typedef	typename allocator_type::pointer			pointer;
+			typedef	typename allocator_type::const_pointer		const_pointer;
+			typedef typename allocator_type::size_type			size_type;
+			
+			// My Members Types
+			typedef node<const Key, T> *								nodePtr;
+			typedef std::allocator<node<const key_type, mapped_type> > nodeAlloc;
+		
+		public:
+			nodePtr		_root;
+			Compare		_comp;
+			Alloc		_alloc;
+				
+		public:
+			/* ---------------------------------------------------- */
+			/*                                                      */
+			/*                     CONSTRUCTORS                     */
+			/*                                                      */
+			/* ---------------------------------------------------- */
+			tree (const key_compare& comp = key_compare(),const allocator_type& alloc = allocator_type())
+			:  _root(nullptr), _comp(comp), _alloc(alloc)
+			{
+				
+			}
+			
+			~tree()
+			{
+				//this->printTree(this->_root, nullptr, false, 0);
+			}
+		
+			/* ---------------------------------------------------- */
+			/*                                                      */
+			/*                     UTILS                            */
+			/*                                                      */
+			/* ---------------------------------------------------- */
+			//Return a empty node NOT USED
+			nodePtr	new_node()
+			{
+				nodeAlloc	alloc;
+				nodePtr		node_ptr;
+				
+				node_ptr = alloc.allocate(1);
+				alloc.construct(node_ptr, node<const Key, T>());
+				return (node_ptr);
+			}
+
+			//New node with pair param
+			nodePtr	new_node(const ft::pair<const Key, T>& val)
+			{
+				nodeAlloc	alloc;
+				nodePtr		node_ptr;
+				ft::node<const Key, T>    node(val);
+				
+				node_ptr = alloc.allocate(1);
+				alloc.construct(node_ptr, node);
+				return (node_ptr);
+			}
+
+			//Return smallest node
+			nodePtr down_smallest_node(nodePtr src) const
+			{
+				nodePtr tmp = src;
+
+				while (tmp->left)
+					tmp = tmp->left;
+				return tmp;
+			}
+			
+			void insert (const value_type& val)
+			{
+				if (!this->_root)
+					this->_root = new_node(val);
+				else
+				{
+					nodePtr tmp = this->_root;
+					
+					while (true)
+					{
+						if (this->_comp(tmp->data->first, val.first))
+						{
+							if (!tmp->right)
+							{
+								tmp->right = new_node(val);
+								tmp->right->parent = tmp;
+								break;
+							}
+							else
+								tmp = tmp->right;
+						}
+						else
+						{
+							if (!tmp->left)
+							{
+								tmp->left = new_node(val);
+								tmp->left->parent = tmp;
+								break;
+							}
+							else
+							tmp = tmp->left;
+						}
+					}
+				}
+			}
+			
+			//Return true if something is erased
+			bool erase (const key_type& k)
+			{
+				nodePtr tmp = this->search(k);
+				
+				if (!this->_root || !tmp) //No Tree or k is not in tree
+				 	return false;
+
+				if (tmp == this->_root) //erase root
+				{
+					if (!tmp->left && !tmp->right)
+						this->_root = nullptr;
+					else if (!tmp->left)
+					{
+						tmp->right->parent = nullptr;
+						this->_root = tmp->right;
+					}
+					else if (!tmp->right)
+					{
+						tmp->left->parent = nullptr;
+						this->_root = tmp->left;
+					}
+					else
+					{
+						tmp->right->parent = nullptr;
+						tmp->left->parent = this->down_smallest_node(tmp->right); //DO NOT CHANGE ORDER
+						(this->down_smallest_node(tmp->right))->left = tmp->left;
+						this->_root = tmp->right;
+						tmp = nullptr;
+					}
+				 	return true;
+				}
+				
+				bool side = this->side(tmp, tmp->parent); //false: parent->left = tmp, true: parent->right = tmp; 
+				if (!tmp->left && !tmp->right) //erase leaf
+				{
+					if (!side)
+						tmp->parent->left = nullptr;
+					else
+						tmp->parent->right = nullptr;
+					tmp = nullptr;
+					return true;
+				}
+				else if (!tmp->left || !tmp->right)  //erase single node
+				{
+					if (!tmp->left)
+					{
+						tmp->right->parent = tmp->parent;
+						if (!side)//parent->left = tmp
+							tmp->parent->left = tmp->right;
+						else//parent->right = tmp
+							tmp->parent->right = tmp->right;
+					}
+					else
+					{
+						tmp->left->parent = tmp->parent;
+						if (!side)
+							tmp->parent->left = tmp->left;
+						else
+							tmp->parent->right = tmp->left;
+					}
+					return true;
+				}
+				else
+				{
+					tmp->right->parent = tmp->parent;
+					tmp->left->parent = this->down_smallest_node(tmp->right); //DO NOT CHANGE ORDER
+					(this->down_smallest_node(tmp->right))->left = tmp->left;
+					if (!side)
+						tmp->parent->left = tmp->right;
+					else
+						tmp->parent->right = tmp->right;
+					return true;
+				}
+				return false;
+			}
+
+			//Return side of the child compared to parent
+			bool side(nodePtr child, nodePtr parent) const
+			{
+				if (parent->left == child)
+					return false; //left
+				return true; //right
+			}
+
+			/* ---------------------------------------------------- */
+			/*                                                      */
+			/*                      CAPACITY                        */
+			/*                                                      */
+			/* ---------------------------------------------------- */
+			bool empty() const
+			{
+				return (!this->_root ? true : false);
+			}
+
+			/* ---------------------------------------------------- */
+			/*                                                      */
+			/*                      FIND                            */
+			/*                                                      */
+			/* ---------------------------------------------------- */
+			nodePtr ret_root() const
+			{
+				return (this->_root);
+			}
+			
+			//Return Smallest Node
+			nodePtr begin() const//IF NOT HERE BUG
+			{
+				nodePtr tmp = this->_root;
+				
+				if (!tmp)
+					return (nullptr);
+				if (!tmp->left)
+					return (tmp);
+				while (tmp->left)
+				{
+					tmp = tmp->left;
+				}
+				return (tmp);
+			}
+			
+			//Return Biggest Node
+			nodePtr end() const
+			{
+				nodePtr tmp = this->_root;
+				
+				if (!tmp)
+					return (nullptr);
+				if (!tmp->right)
+					return (tmp);
+				while (tmp->right)
+				{
+					tmp = tmp->right;
+				}
+				return (tmp);
+			}
+			
+			//Return node or nullptr if not found
+			nodePtr	search(const key_type& k) const
+			{
+				nodePtr tmp = this->_root;
+				
+				if (!tmp)
+					return (nullptr);
+		
+				while (tmp) 
+				{
+					if (this->_comp(tmp->data->first, k) && tmp->right) //bigger
+						tmp = tmp->right;
+					else if (this->_comp(k, tmp->data->first) && tmp->left) //smaller
+						tmp = tmp->left;
+					else if (this->_comp(k, tmp->data->first) == false && this->_comp(tmp->data->first, k) == false) //equal
+						break;
+					else //tmp not found
+						tmp = nullptr;
+				}
+				return (tmp);
+			}
+			
+			
+			/* ---------------------------------------------------- */
+			/*                                                      */
+			/*                     OBSERVERS                        */
+			/*                                                      */
+			/* ---------------------------------------------------- */
+			key_compare key_comp() const
+			{
+				return (this->_comp);
+			}
+
+			class value_compare
+			{
+				friend class tree;
+				protected:
+
+					Compare comp;
+					value_compare (Compare c) : comp(c) {}
+
+				public:
+
+					typedef bool result_type;
+					typedef value_type first_argument_type;
+					typedef value_type second_argument_type;
+					bool operator() (const value_type& x, const value_type& y) const
+					{
+						return comp(x.first, y.first);
+					}
+			};
+
+			value_compare value_comp() const
+			{
+				return (value_compare(_comp));
+			}
+
+			/* ---------------------------------------------------- */
+			/*                                                      */
+			/*                      DEBUG		                    */
+			/*                                                      */
+			/* ---------------------------------------------------- */
+			// printTree(this->_root, nullptr, false, 0);
+			// struct Trunk
+			// {
+			// 	Trunk *prev;
+			// 	std::string str;
+
+			// 	Trunk(Trunk *prev, std::string str)
+			// 	{
+			// 		this->prev = prev;
+			// 		this->str = str;
+			// 	}
+			// };
+
+			// void showTrunks(Trunk *p)
+			// {
+			// 	if (p == NULL) {
+			// 		return;
+			// 	}
+
+			// 	showTrunks(p->prev);
+			// 	std::cout << p->str;
+			// }
+
+			// void printTree(node<const Key, T>* root, Trunk *prev, bool isLeft, bool type)
+			// {
+			// 	if (root == NULL) {
+			// 		return;
+			// 	}
+
+			// 	std::string prev_str = "    ";
+			// 	Trunk *trunk = new Trunk(prev, prev_str);
+
+			// 	printTree(root->right, trunk, true, type);
+			// 	if (!prev) {
+			// 		trunk->str = "———";
+			// 	}
+			// 	else if (isLeft)
+			// 	{
+			// 		trunk->str = "/———";
+			// 		prev_str = "   |";
+			// 	}
+			// 	else {
+			// 		trunk->str = "\\———";
+			// 		prev->str = prev_str;
+			// 	}
+			// 	showTrunks(trunk);
+			// 	std::cout << " " << root->data->first << "( ): " << root->data->second << std::endl;
+			// 	if (prev) {
+			// 		prev->str = prev_str;
+			// 	}
+			// 	trunk->str = "   |";
+			// 	printTree(root->left, trunk, false, type);
+			// }
+	};
+}
+
+
+#endif
